a099aa134463205dc4a1b29f8dd041a96b3554c2
Natasha1324 <165684802+Natasha1324@users.noreply.github.com>
2024-04-02 00:50:47 -1200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_No._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter seconds: ");
+            int seconds = Convert.ToInt32(Console.ReadLine());
+
+            //Convert seconds to minutes
+            double minutes = seconds / 60.0;
+
+            //Convert minutes to hours
+            double hours = minutes / 60.0;
+            
+            //Convert hours to days
+            double days = hours / 24.0;
+            
+            //Convert days to months
+            double months = days / 30.0;
+
+            Console.WriteLine($" {seconds} seconds equal to: ");
+            Console.WriteLine($" {minutes} minutes");
+            Console.WriteLine($" {hours} hours");
+            Console.WriteLine($" {days} days");
+            Console.WriteLine($" {months} months");
+        }
+
+    }
+}
+                           
